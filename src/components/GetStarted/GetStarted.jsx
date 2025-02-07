1fb1248ffcae5bdbb95ae6c0a6b249ef8902f9d6
Anusha Shetty <anushasshetty200@gmail.com>
2025-02-07 21:04:14 +0530

    Estate

---
+++ src/components/GetStarted/GetStarted.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import './GetStarted.css'
+
+const GetStarted = () => {
+  return (
+    <section className="gs-wrapper">
+        <div className="padding innerWidth g-container">
+            <div className="flexColCenter inner-container">
+                <span className="primaryText">Get Started With Homyc</span>
+                <span className="secondaryText">
+                    Subscribe and Find super attractive deals on your next home
+                    <br />
+                    Find your Residence Soon
+                </span>
+                <button className="button">
+                    <a href="mailto:anushasshetty200@gmai.com">Get Started </a>
+                </button>
+                </div>
+                </div>
+        </section>
+  )
+}
+
+export default GetStarted
